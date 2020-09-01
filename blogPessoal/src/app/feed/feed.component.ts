b08d3381f93b2898120dc2683505892cf2a07f78
Victorhup <victorhup95@hotmail.com>
2020-09-01 16:24:07 -0300

    Inserindo método get

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,8 @@
+import { TemaService } from './../service/tema.service';
+import { PostagemService } from './../service/postagem.service';
+import { Postagem } from './../model/Postagem';
 import { Component, OnInit } from '@angular/core';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-feed',
@@ -6,11 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  
+  postagem: Postagem = new Postagem ()
+  listaPostagens: Postagem[]
+  idTema: number
+  
+  tema: Tema = new Tema ()
+  listaTemas: Tema []
 
-  constructor() { }
+  constructor(
+    private postagemService: PostagemService,
+    private temaService: TemaService
+  ) { }
 
   ngOnInit(){
     window.scroll(0,0)
+
+    this.findAllPostagens()
+    this.findAllTemas()
   }
 
+  findAllPostagens (){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+  }
+
+  findAllTemas (){
+    this.temaService.getAllTemas().subscribe((resp: Tema []) =>{
+      this.listaTemas = resp
+    }) 
+  }
+  
+  findByIdTema () {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp;
+    })
+  }
 }
